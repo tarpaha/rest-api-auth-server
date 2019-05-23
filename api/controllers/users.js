678a199401ef05c4ca7f23424206c12afc363050
tarpaha <ppopchikovsky@gmail.com>
2019-05-23 21:25:27 +0300

    user controller with createTest() function

---
+++ api/controllers/users.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const logger = require('../../libs/logger');
+const UserModel = require('../models/users');
+
+function createTest(req, res) {
+    logger.info('-> users.createTest()');
+    var user = new UserModel({
+        name: 'name',
+        email: 'email',
+        password: 'password'
+    });
+    user.save(function(err) {
+        if(err) {
+            logger.error('-> users.createTest() error: ' + err.message);
+            res.statusCode = 500;
+            return res.json({
+                status: 'error',
+                error: 'Server error'
+            });
+        }
+        logger.info('-> users.createTest() successful, id = ' + user._id);
+        res.json({ status: 'success', user: { id: user._id }});
+    });
+}
+
+module.exports = { createTest }
