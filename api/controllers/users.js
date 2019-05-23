0265e8966ffcb8088dedbf90b5c096405b1b08c5
tarpaha <ppopchikovsky@gmail.com>
2019-05-23 21:58:19 +0300

    users.create() was extracted from createTest()

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -3,25 +3,32 @@
 const logger = require('../../libs/logger');
 const UserModel = require('../models/users');
 
-function createTest(req, res) {
-    logger.info('-> users.createTest()');
+function create(req, res) {
+    logger.info('-> users.create()');
     var user = new UserModel({
-        name: 'name',
-        email: 'email',
-        password: 'password'
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
     });
     user.save(function(err) {
         if(err) {
-            logger.error('-> users.createTest() error: ' + err.message);
+            logger.error('-> users.create() error: ' + err.message);
             res.statusCode = 500;
             return res.json({
                 status: 'error',
                 error: err.code == 11000 ? 'Duplicate email' : 'Server error'
             });
         }
-        logger.info('-> users.createTest() successful, id = ' + user._id);
+        logger.info('-> users.create() successful, id = ' + user._id);
         res.json({ status: 'success', user: { id: user._id }});
     });
+}
+
+function createTest(req, res) {
+    req.body.name = 'name';
+    req.body.email = 'email';
+    req.body.password = 'password';
+    create(req, res);
 }
 
 module.exports = { createTest }
