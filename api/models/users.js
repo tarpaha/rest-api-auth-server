b4eb4dbcb64d80fa89a1aa44f98a1d5588b53b66
tarpaha <ppopchikovsky@gmail.com>
2019-05-23 21:21:10 +0300

    user model with encrypted password

---
+++ api/models/users.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const UserSchema = mongoose.Schema({
+    name: { type: String, trim: true, required: true },
+    email: { type: String, trim: true, required: true },
+    password: { type: String, trim: true, required: true }
+});
+
+UserSchema.pre('save', function(next) {
+    this.password = bcrypt.hashSync(this.password, saltRounds);
+    next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
