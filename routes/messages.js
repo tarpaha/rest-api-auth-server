e14e353f8889fe9a78fe24c86e5949141e42b40d
tarpaha <ppopchikovsky@gmail.com>
2019-05-28 19:14:20 +0300

    jwt token required to create test message

--- routes/messages.js
+++ routes/messages.js
@@ -1,9 +1,29 @@
 'use strict';
 
+const jwt = require('jsonwebtoken');
+const logger = require('../libs/logger');
+const config = require('../libs/config');
+
 const router = require('express').Router();
 const messagesController = require('../api/controllers/messages');
 
 router.get('/', messagesController.getAll);
-router.get('/test', messagesController.createTest);
+router.get('/test', validateUser, messagesController.createTest);
+
+function validateUser(req, res, next) {
+    logger.info('-> validateUser()')
+    jwt.verify(req.headers['x-access-token'], config.get('jwt:key'), function(err, decoded) {
+        if(err) {
+            res.statusCode = 500;
+            res.json({
+                status: 'error',
+                error: err.message
+            });
+        }
+        logger.info('-> validateUser() success, id = ' + decoded.id);
+        req.userId = decoded.id;
+        next();
+    });
+}
 
 module.exports = router;
